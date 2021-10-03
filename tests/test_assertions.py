2180d5bfa0451fb815ace589bd40ec5a024ae150
Naveen Anil <naveen.anil@wind-pioneers.com>
2021-10-03 17:57:12 +0530

    OPS: Fix the package for release

--- tests/test_assertions.py
+++ tests/test_assertions.py
@@ -23,6 +23,30 @@ class TestBasicAssertions(unittest.TestCase):
         with self.assertRaises(WrongTimeComplexity):
             assertBigO(max_function, input_generator, Quadratic)
 
+    def test_quadratic_complexity(self):
+        def bad_max_function(number_list):
+            max_ = 0
+            for el in number_list:
+                if el > max_:
+                    max_ = el
+
+                # To bump the time complexity to n^2
+                for el in number_list:
+                    pass
+
+            return max_
+
+        def input_generator(numbers):
+            return list(range(numbers))
+
+        with self.assertRaisesRegex(WrongTimeComplexity, "Expected 'Linear' but fitted time complexity is 'Quadratic'"):
+            assertBigO(
+                bad_max_function,
+                input_generator,
+                Linear,
+                timer_options={"minimum_n": 100, "maximum_n": 1000, "n_count": 10},
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
